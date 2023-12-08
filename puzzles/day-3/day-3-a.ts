40a74594ec5874eabfd623e3f8772c3d6fd3d5b2
Philipp <philipp@bitfactory.io>
2023-12-08 20:46:22 +0100

    day-3-A + day-3-B

--- puzzles/day-3/day-3-a.ts
+++ puzzles/day-3/day-3-a.ts
@@ -1,9 +1,87 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+type FoundNumber = {
+  value: number;
+  start: number;
+  end: number;
+  line: number;
+}
+
+type Symbol = {
+  index: number,
+  line: number,
+}
+
 export async function day3a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const foundNumbers: FoundNumber[] = [];
+  const foundSymbols: Symbol[] = []
+
+  const matchSymbolsRegExp = /[^0-9.]/
+
+  const numbersInDataString = (str: string, line: number) => {
+    let i = 0;
+    while(i < str.length) {
+      const char = str[i];
+      const isNumber = !isNaN(parseInt(char));
+
+      if (isNumber) {
+        let currentNumString = str[i];
+        let j = i + 1;
+
+        while (!isNaN(parseInt(str[j])) && str[i] !== '.') {
+          currentNumString += str[j];
+          j++;
+        }
+
+        console.log(currentNumString, i, j)
+        foundNumbers.push({
+          value: parseInt(currentNumString),
+          start: i > 0 ? i - 1 : i,
+          end: j,
+          line,
+        })
+        i = j;
+      } else {
+        i++;
+      }
+    }
+  }
+
+  const symbolsInDataString = (str: string, line: number) => {
+    for (let i = 0; i < str.length; i++) {
+      const isSymbol = matchSymbolsRegExp.test(str[i]);
+      if (isSymbol) {
+        foundSymbols.push({
+          index: i,
+          line
+        })
+      }
+    }
+  }
+
+  data.forEach((str, index) => {
+    numbersInDataString(str, index);
+    symbolsInDataString(str, index);
+  });
+
+
+  const checkSymbols = (num: FoundNumber): boolean => {
+    return !!foundSymbols.find(symbol => {
+      const matchesIndex = symbol.index >= num.start && symbol.index <= num.end;
+      const matchesLine = symbol.line + 1 === num.line || symbol.line - 1 === num.line || symbol.line === num.line;
+
+      return matchesIndex && matchesLine;
+    })
+  }
+
+  const result = foundNumbers.map((num, currentLineIndex) => {
+    return checkSymbols(num) ? num.value : 0;
+  })
+
+  return result.reduce((acc, num) => acc + num, 0);
 }
 
 const answer = await day3a();
--- puzzles/day-3/day-3-b.ts
+++ puzzles/day-3/day-3-b.ts
@@ -1,9 +1,97 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+type FoundNumber = {
+  value: number;
+  start: number;
+  end: number;
+  line: number;
+}
+
+type GearSymbol = {
+  index: number,
+  line: number,
+}
+
 export async function day3b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+
+  const foundNumbers: FoundNumber[] = [];
+  const foundSymbols: GearSymbol[] = []
+
+  const matchSymbolsRegExp = /[*]/
+
+  const numbersInDataString = (str: string, line: number) => {
+    let i = 0;
+    while(i < str.length) {
+      const char = str[i];
+      const isNumber = !isNaN(parseInt(char));
+
+      if (isNumber) {
+        let currentNumString = str[i];
+        let j = i + 1;
+
+        while (!isNaN(parseInt(str[j])) && str[i] !== '.') {
+          currentNumString += str[j];
+          j++;
+        }
+
+        foundNumbers.push({
+          value: parseInt(currentNumString),
+          start: i > 0 ? i - 1 : i,
+          end: j,
+          line,
+        })
+        i = j;
+      } else {
+        i++;
+      }
+    }
+  }
+
+  const symbolsInDataString = (str: string, line: number) => {
+    for (let i = 0; i < str.length; i++) {
+      const isSymbol = matchSymbolsRegExp.test(str[i]);
+      if (isSymbol) {
+        foundSymbols.push({
+          index: i,
+          line
+        })
+      }
+    }
+  }
+
+  data.forEach((str, index) => {
+    numbersInDataString(str, index);
+    symbolsInDataString(str, index);
+  });
+
+  const checkNumbers = (symbol: GearSymbol): number[]  => {
+    const result = foundNumbers.filter(( num) => {
+      const matchesIndex = symbol.index >= num.start && symbol.index <= num.end;
+      const matchesLine = symbol.line + 1 === num.line || symbol.line - 1 === num.line || symbol.line === num.line;
+      return matchesIndex && matchesLine;
+    })
+
+    if (result.length < 2) {
+      return [];
+    }
+    return result.map(res => res.value);
+  }
+
+  const result = foundSymbols.map((symbol, currentLineIndex) => {
+    return checkNumbers(symbol);
+  })
+
+
+
+  return result.reduce((acc, num) => {
+    if (num.length) {
+      acc += num.reduce((accNum, num2) => accNum * num2, 1);
+    }
+    return acc;
+  }, 0);
 }
 
 const answer = await day3b();
