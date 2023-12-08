758f1ccedb0713aefcfe66444e53f2ff334a120e
Philipp <philipp@bitfactory.io>
2023-12-08 21:31:36 +0100

    day-4-A

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -2,8 +2,34 @@ import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
 export async function day4a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const scratchCards = await readData(dataPath);
+
+  let totalPoints = 0;
+
+  const extractNumbersFromString = (str: string): number[] => {
+    return str.split(' ').filter(result => {
+      if (!isNaN(parseInt(result))) {
+        return parseInt(result);
+      }
+    }).map(res => parseInt(res));
+  }
+
+  scratchCards.forEach(scratchCard => {
+    if (!scratchCard.length) {
+      return;
+    }
+
+    const [, winningStr, availableStr] = scratchCard.split(/[:|]/);
+    const [winning, available] = [extractNumbersFromString(winningStr), extractNumbersFromString(availableStr)]
+
+    const amountOfWinners = winning.filter(winner => available.includes(winner)).length;
+
+    if (amountOfWinners) {
+      totalPoints += Math.pow(2, amountOfWinners - 1);
+    }
+  })
+
+  return totalPoints;
 }
 
 const answer = await day4a();
